968388843a54e6b1ba5582cf75a063ee2a5d12b9
GZY-q <2965753496@qq.com>
2025-08-16 16:02:33 +0800

    创建viewer项目
    start

---
+++ MarkdownViewer/MarkdownViewer/MarkdownViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarkdownViewerApp.swift
+//  MarkdownViewer
+//
+//  Created by 贡正阳 on 2025/8/16.
+//
+
+import SwiftUI
+
+@main
+struct MarkdownViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
